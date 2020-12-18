3761f451747d69b7b392770b820455cff21cd229
Wil Taylor <cert@wiltaylor.dev>
2020-12-18 16:41:29 +1000

    Initial Version

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "My personal NIX PKGS repo";
+
+  inputs = {
+    # Upstream nixpkgs packages loaded from here
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-20-09.url = "github:nixos/nixpkgs/nixos-20.09";
+
+    #Put all category repositories here
+  };
+
+  outputs = inputs:
+  let
+    system = "x86_64-linux";
+  in {
+
+    nixpkgs = {
+      master = inputs.nixpkgs-master.legacyPackages."${system}";
+      unstable = inputs.nixpkgs-unstable."${system}";
+      "20-09" = inputs.nixpkgs-20-09."${system}";
+    };
+
+  };
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# SYSPKGS
+
+This is my nix package abstraction repository. For the most part it just points to various branches of the nixpkgs repo.
+
+Will flesh this out more later.
