7beb6da502856584deeba9395b5d39c31a29abf3
Wil Taylor <cert@wiltaylor.dev>
2020-12-18 19:32:34 +1000

    Added stdenv

--- lib/default.nix
+++ lib/default.nix
@@ -7,4 +7,8 @@
     hasAttr head isAttrs isBool isInt isList isString length
     lessThan listToAttrs pathExists readFile replaceStrings seq
     stringLength sub substring tail trace;
+
+  stdenv = lib.stdenv;
+
+
 }
